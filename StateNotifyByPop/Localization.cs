df4987b7029f2ffddbeb56de8fa548b7f7eb170a
Quartz <yyhran@163.com>
2025-11-09 17:38:06 +0800

    format code

--- StateNotifyByPop/Localization.cs
+++ StateNotifyByPop/Localization.cs
@@ -33,14 +33,14 @@ namespace StateNotifyByPop
             // Chinese Traditional
             var zh_tw = new Dictionary<string, string>
             {
-                {"SNBP_Water_Stage1", "感覺有點渴"},
-                {"SNBP_Water_Stage2", "感覺非常口渴"},
-                {"SNBP_Water_Stage3", "快要渴死了"},
-                {"SNBP_Energy_Stage1", "感覺有點餓"},
-                {"SNBP_Energy_Stage2", "感覺非常餓"},
-                {"SNBP_Energy_Stage3", "快要餓死了"},
-                {"SNBP_BothConnector", "，還"},
-                {"SNBP_BothSimple", "我現在又渴又餓"}
+                { "SNBP_Water_Stage1", "感覺有點渴" },
+                { "SNBP_Water_Stage2", "感覺非常口渴" },
+                { "SNBP_Water_Stage3", "快要渴死了" },
+                { "SNBP_Energy_Stage1", "感覺有點餓" },
+                { "SNBP_Energy_Stage2", "感覺非常餓" },
+                { "SNBP_Energy_Stage3", "快要餓死了" },
+                { "SNBP_BothConnector", "，還" },
+                { "SNBP_BothSimple", "我現在又渴又餓" }
             };
             translations[SystemLanguage.ChineseTraditional] = zh_tw;
 
@@ -61,28 +61,28 @@ namespace StateNotifyByPop
             // Japanese
             var ja = new Dictionary<string, string>
             {
-                {"SNBP_Water_Stage1", "ちょっと喉が渇いた"},
-                {"SNBP_Water_Stage2", "とても喉が渇いた"},
-                {"SNBP_Water_Stage3", "もうすぐ干からびそうだ"},
-                {"SNBP_Energy_Stage1", "ちょっとお腹が空いた"},
-                {"SNBP_Energy_Stage2", "とてもお腹が空いた"},
-                {"SNBP_Energy_Stage3", "もうすぐ餓死しそうだ"},
-                {"SNBP_BothConnector", "、そして"},
-                {"SNBP_BothSimple", "喉が渇いてお腹が空いている"}
+                { "SNBP_Water_Stage1", "ちょっと喉が渇いた" },
+                { "SNBP_Water_Stage2", "とても喉が渇いた" },
+                { "SNBP_Water_Stage3", "もうすぐ干からびそうだ" },
+                { "SNBP_Energy_Stage1", "ちょっとお腹が空いた" },
+                { "SNBP_Energy_Stage2", "とてもお腹が空いた" },
+                { "SNBP_Energy_Stage3", "もうすぐ餓死しそうだ" },
+                { "SNBP_BothConnector", "、そして" },
+                { "SNBP_BothSimple", "喉が渇いてお腹が空いている" }
             };
             translations[SystemLanguage.Japanese] = ja;
 
             // Korean
             var ko = new Dictionary<string, string>
             {
-                {"SNBP_Water_Stage1", "약간 목이 마르다"},
-                {"SNBP_Water_Stage2", "매우 목이 마르다"},
-                {"SNBP_Water_Stage3", "목말라 죽을 것 같다"},
-                {"SNBP_Energy_Stage1", "약간 배고프다"},
-                {"SNBP_Energy_Stage2", "매우 배고프다"},
-                {"SNBP_Energy_Stage3", "거의 굶어 죽을 것 같다"},
-                {"SNBP_BothConnector", " 그리고 "},
-                {"SNBP_BothSimple", "목이 마르고 배고프다"}
+                { "SNBP_Water_Stage1", "약간 목이 마르다" },
+                { "SNBP_Water_Stage2", "매우 목이 마르다" },
+                { "SNBP_Water_Stage3", "목말라 죽을 것 같다" },
+                { "SNBP_Energy_Stage1", "약간 배고프다" },
+                { "SNBP_Energy_Stage2", "매우 배고프다" },
+                { "SNBP_Energy_Stage3", "거의 굶어 죽을 것 같다" },
+                { "SNBP_BothConnector", " 그리고 " },
+                { "SNBP_BothSimple", "목이 마르고 배고프다" }
             };
             translations[SystemLanguage.Korean] = ko;
 
